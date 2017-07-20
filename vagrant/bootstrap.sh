2f88d4083888fb73d061fbb4526fc14ba0e7fbac
Szkered <emperorview@gmail.com>
2017-07-20 17:16:01 +0800

    new constellation binary and geth retry hot-fix

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,14 +5,21 @@ set -eu -o pipefail
 # install build deps
 add-apt-repository ppa:ethereum/ethereum
 apt-get update
-apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk
+apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk libleveldb-dev
+
+# apt-get install -y npm nodejs-legacy
+
+# install netting dapps
+# npm install -g truffle
 
 # install constellation
-wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip
-unzip ubuntu1604.zip
-cp ubuntu1604/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
-cp ubuntu1604/constellation-enclave-keygen /usr/local/bin && chmod 0755 /usr/local/bin/constellation-enclave-keygen
-rm -rf ubuntu1604.zip ubuntu1604
+# wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip
+# unzip ubuntu1604.zip
+# cp ubuntu1604/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
+# cp ubuntu1604/constellation-enclave-keygen /usr/local/bin && chmod 0755 /usr/local/bin/constellation-enclave-keygen
+# rm -rf ubuntu1604.zip ubuntu1604
+cp /vagrant/examples/constellation-node /usr/local/bin/constellation-node
+
 
 # install golang
 GOREL=go1.7.3.linux-amd64.tar.gz
@@ -28,10 +35,16 @@ git clone https://github.com/jpmorganchase/quorum.git
 pushd quorum >/dev/null
 # git checkout tags/v1.1.0
 git checkout master
+
+# geth retry patch
+cd /home/ubuntu/quorum
+git apply /vagrant/examples/retry.diff
+
 make all
 cp build/bin/geth /usr/local/bin
 cp build/bin/bootnode /usr/local/bin
 popd >/dev/null
+
 
 # copy examples
 cp -r /vagrant/examples /home/ubuntu/quorum-examples
